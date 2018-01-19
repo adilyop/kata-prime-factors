1c9b97d713120cbf0d17542464f91bbddf529533
Adil GUEZZI <aguezzi@Adils-MacBook-Pro.local>
2018-01-19 15:53:43 +0100

    return prime list when input < 9

--- app/primeFactors.js
+++ app/primeFactors.js
@@ -2,7 +2,15 @@
 class primeFactore {
 
     generate(input) {
-        let result = []
+        let result = [];
+        if (input > 1) {
+            while (input % 2 === 0) {
+                result.push(2);
+                input = input / 2;
+            }
+            if (input > 1)
+                result.push(input)
+        }
         return result;
     }
 }
--- test/primeFactors.test.js
+++ test/primeFactors.test.js
@@ -10,4 +10,38 @@ describe('prime factors Kata Test: \n', () => {
         expect(primeFactors.generate(1)).to.be.an('array')
         expect(primeFactors.generate(1)).to.be.empty;
     });
+    it("should return empty table when input is 2", function () {
+        expect(primeFactors.generate(2)).to.be.an('array')
+        expect(primeFactors.generate(2)).to.have.lengthOf(1);
+        expect(primeFactors.generate(2)[0]).to.equal(2);
+    });
+    it("should return empty table when input is 3", function () {
+        expect(primeFactors.generate(3)).to.be.an('array')
+        expect(primeFactors.generate(3)).to.have.lengthOf(1);
+        expect(primeFactors.generate(3)[0]).to.equal(3);
+    });
+    it("should return empty table when input is 4", function () {
+        expect(primeFactors.generate(4)).to.be.an('array')
+        expect(primeFactors.generate(4)).to.have.lengthOf(2);
+        expect(primeFactors.generate(4)[0]).to.equal(2);
+        expect(primeFactors.generate(4)[1]).to.equal(2);
+    });
+    it("should return empty table when input is 5", function () {
+        expect(primeFactors.generate(5)).to.be.an('array')
+        expect(primeFactors.generate(5)).to.have.lengthOf(1);
+        expect(primeFactors.generate(5)[0]).to.equal(5);
+    });
+    it("should return empty table when input is 6", function () {
+        expect(primeFactors.generate(6)).to.be.an('array')
+        expect(primeFactors.generate(6)).to.have.lengthOf(2);
+        expect(primeFactors.generate(6)[0]).to.equal(2);
+        expect(primeFactors.generate(6)[1]).to.equal(3);
+    });
+    it("should return empty table when input is 8", function () {
+        expect(primeFactors.generate(8)).to.be.an('array')
+        expect(primeFactors.generate(8)).to.have.lengthOf(3);
+        expect(primeFactors.generate(8)[0]).to.equal(2);
+        expect(primeFactors.generate(8)[1]).to.equal(2);
+        expect(primeFactors.generate(8)[1]).to.equal(2);
+    });
 });
